1aa1ab6bd546daf7ce7dacbe8d9bdeb7bcf128f4
Petr Krysl <pkrysl@ucsd.edu>
2020-02-13 08:30:33 -0800

    oorganization

--- src/FinEtoolsFrames.jl
+++ src/FinEtoolsFrames.jl
@@ -1,19 +1,11 @@
 module FinEtoolsFrames
 
+__precompile__(false)
+
 using FinEtools
 
-mutable struct FESetL2CorotBeam <: AbstractFESet1Manifold{2}
-    conn::Array{NTuple{2, FInt}, 1};
-    label::FIntVec; 
-    function FESetL2CorotBeam(conn::FIntMat)
-        self = new(NTuple{2, FInt}[], FInt[])
-        self = fromarray!(self, conn)
-        setlabel!(self, 0)
-        return self
-    end
-end
-
 include("CrossSectionModule.jl")
+include("FESetCorotBeamModule.jl")
 include("MeshFrameMemberModule.jl")
 
 end # module
