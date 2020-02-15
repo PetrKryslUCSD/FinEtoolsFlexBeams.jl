21d830fa805de1e6d9456ad2ee7281eafd45c658
Petr Krysl <pkrysl@ucsd.edu>
2020-02-14 16:53:16 -0800

    use beam visualization package

--- examples/meshing/mesh_examples.jl
+++ examples/meshing/mesh_examples.jl
@@ -1,24 +1,12 @@
-module m1
-using FinEtools
-using FinEtoolsFlexBeams.FESetCorotBeamModule: FESetL2CorotBeam
-using FinEtoolsFlexBeams.CrossSectionModule: CrossSectionCircle
-using Test
-function test()
-    cs = CrossSectionCircle(s -> 5.9910, s -> [0.0, 0.0, 1.0])
-    fes = FESetL2CorotBeam([1 2; 2 3; 3 4], cs)
-end
-end
-using .m1
-m1.test()
+module mesh_examples
 
-module m2
 using FinEtools
 using FinEtoolsFlexBeams.FESetCorotBeamModule: FESetL2CorotBeam
 using FinEtoolsFlexBeams.CrossSectionModule: CrossSectionCircle
 using FinEtoolsFlexBeams.MeshFrameMemberModule: frame_member
-using PlotlyJS
-using Test
-function test()
+using FinEtoolsBeamsVis: plot_nodes, render
+
+function curve_mesh()
     L = 42
     xyz = [0 0 0;
     0 L/4 L*1/4;
@@ -29,14 +17,18 @@ function test()
     
     cs = CrossSectionCircle(s -> 5.9910, s -> [0.0, 0.0, 1.0])
     fens, fes = frame_member(xyz, nL, cs)
-    nodes = scatter3d(;x=fens.xyz[:, 1],y=fens.xyz[:, 2], z=fens.xyz[:, 3], mode="lines",
-        marker=attr(color="#1f77b4", size=12, symbol="circle",
-            line=attr(color="rgb(0,0,0)", width=3)),
-        line=attr(color="#1f77b4", width=4))
-    layout = Layout(autosize=false, width=500, height=500,
-        margin=attr(l=0, r=0, b=0, t=65))
-    display(plot([nodes], layout))
-end
-end
-using .m2
-m2.test()
+    plots = [plot_nodes(fens)]
+    # push!(plots, plot_nodes(fens))
+    render(plots)
+    true
+end # curve_mesh
+
+function allrun()
+    println("#####################################################")
+    println("# curve_mesh ")
+    curve_mesh()
+    return true
+end # function allrun
+
+end # module
+
