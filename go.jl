b4dc81c3747ad97ae119ed5ee51a8cde740bbfae
Petr Krysl <pkrysl@ucsd.edu>
2020-02-17 10:37:56 -0800

    helpers

--- go.jl
+++ go.jl
@@ -2,17 +2,20 @@
 using Pkg; Pkg.activate("."); Pkg.instantiate()
 
 
-# include(".\\examples\\meshing\\mesh_examples.jl");   
+# include(".\\examples\\meshing\\mesh_examples.jl");
 # mesh_examples.curve_mesh_envelope()
 # mesh_examples.argyr_l_frame_envelope()
 # mesh_examples.argyr_l_frame_movable()
 
-# include(".\\examples\\modal\\beam_modal_examples.jl"); 
+# include(".\\examples\\modal\\beam_modal_examples.jl");
 # beam_modal_examples.allrun()
 
 
-# include(".\\examples\\modal\\argyr_l_frame_modal_examples.jl");   
+# include(".\\examples\\modal\\argyr_l_frame_modal_examples.jl");
 # argyr_l_frame_modal_examples.allrun()
 
-include(".\\examples\\linear_buckling\\tippling_examples.jl");  
-tippling_examples.tippling_1()
+# include(".\\examples\\linear_buckling\\tippling_examples.jl");
+# tippling_examples.tippling_1()
+
+include(".\\examples\\modal\\argyr_l_frame_modal_examples.jl");
+argyr_l_frame_modal_examples.argyr_l_frame_modal_anim()
