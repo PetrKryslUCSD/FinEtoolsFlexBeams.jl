786cd4f7a86fc1bf3a5d3cb18cde26666554825c
Petr Krysl <pkrysl@ucsd.edu>
2020-02-17 10:37:42 -0800

    First animation

--- examples/modal/argyr_l_frame_modal_examples.jl
+++ examples/modal/argyr_l_frame_modal_examples.jl
@@ -16,7 +16,8 @@ using LinearAlgebra: dot
 using Arpack
 using LinearAlgebra
 using SparseArrays
-using FinEtoolsBeamsVis: plot_nodes, plot_midline, render, plot_space_box, plot_envelope, space_aspectratio
+using FinEtoolsBeamsVis: plot_points, plot_nodes, plot_midline, render, plot_space_box, plot_envelope, space_aspectratio
+using PlotlyJS
 
 function argyr_l_frame_modal()
     # Parameters:
@@ -35,7 +36,7 @@ function argyr_l_frame_modal()
     cs = CrossSectionRectangle(s -> b, s -> h, s -> [0.0, 1.0, 0.0])
 
     # Select the number of elements per leg.
-    xyz = 
+    xyz =
     n=8;
     members = []
     push!(members, frame_member([0 0 L; L 0 L], n, cs))
@@ -74,13 +75,145 @@ function argyr_l_frame_modal()
     # Visualize vibration modes
     scattersysvec!(dchi, v[:, 1])
     update_rotation_field!(Rfield0, dchi)
-    plots = cat(plot_nodes(fens), 
+    plots = cat(plot_nodes(fens),
         plot_envelope(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
         dims = 1)
     render(plots; aspectratio = space_aspectratio(fens.xyz))
 
     return true
 end # argyr_l_frame_modal
+
+function argyr_l_frame_modal_anim()
+    # Parameters:
+    E=71240.0;#MPa
+    nu=0.31;# Poisson ratio
+    rho=5e-9;
+    b=3.0; h=30.0; L=240.0; # cross-sectional dimensions and length of each leg in millimeters
+    # Choose the mass formulation:
+    mass_type=2;
+    scale = 1.0
+
+    # Reference frequencies
+    reffs = [11.2732, 30.5269]
+    neigvs = 2;
+
+    # Cross-sectional properties
+    cs = CrossSectionRectangle(s -> b, s -> h, s -> [0.0, 1.0, 0.0])
+
+    # Select the number of elements per leg.
+    xyz =
+    n=8;
+    members = []
+    push!(members, frame_member([0 0 L; L 0 L], n, cs))
+    push!(members, frame_member([L 0 L; L 0 0], n, cs))
+    fens, fes = merge_members(members; tolerance = L / 10000);
+
+    # Material properties
+    material = MatDeforElastIso(DeforModelRed3D, rho, E, nu, 0.0)
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    l1 = selectnode(fens; box = [0 0 0 0 L L], tolerance = L/10000)
+    for i in [1,2,3,4,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the global discrete system
+    femm = FEMMCorotBeam(IntegDomain(fes, GaussRule(1, 2)), material)
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+    M = mass(femm, geom0, u0, Rfield0, dchi);
+
+    # Solve the eigenvalue problem
+    d,v,nev,nconv = eigs(K, M; nev=2*neigvs, which=:SM)
+    fs = real(sqrt.(complex(d)))/(2*pi)
+    println("Natural frequencies: $fs [Hz]")
+    println("Reference: $fs [Hz]")
+
+    # Visualize vibration modes
+    println("Original")
+    xscale = scale.*sin.(20.0*(2*pi/21))
+    scattersysvec!(dchi, xscale.*v[:, 1])
+    Rfield1 = deepcopy(Rfield0)
+    update_rotation_field!(Rfield1, dchi)
+    plots = cat(plot_space_box([[0 -L 0]; [1.1*L L 1.1*L]]),
+        plot_nodes(fens),
+        plot_envelope(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield1.values, facecolor = "rgb(25, 55, 125)");
+        dims = 1)
+    pl = render(plots)
+
+    sleep(5.0)
+
+    xscale = scale.*sin.(35.0*(2*pi/21))
+    scattersysvec!(dchi, xscale.*v[:, 1])
+    Rfield1 = deepcopy(Rfield0)
+    update_rotation_field!(Rfield1, dchi)
+    # plots = cat(plot_space_box([[0 -L 0]; [1.1*L L 1.1*L]]),
+    #     plot_nodes(fens),
+    #     plot_envelope(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values, facecolor = "rgb(25, 255, 25)");
+    #     dims = 1)
+    # react!(pl, plots, pl.plot.layout)
+    # display(pl)
+
+    # println("Change")
+    # for i in 1:3#1:length(pl.plot.data)
+    #     deletetraces!(pl, i)
+    # end
+    # for tr in plot_envelope(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield1.values, facecolor = "rgb(25, 255, 25)")
+    #     addtraces!(pl, 1, tr)
+    # end
+    # # pl = render(plots)
+    # redraw!(pl)
+
+scale = 0.5
+    for xscale in scale.*sin.(collect(0:1:25).*(2*pi/13))
+        println(xscale)
+        scattersysvec!(dchi, xscale.*v[:, 1])
+        Rfield1 = deepcopy(Rfield0)
+        update_rotation_field!(Rfield1, dchi)
+        println("length(pl.plot.data) = $(length(pl.plot.data))")
+        # plots = cat(plot_space_box([[-L/2 -L/2 0]; [L/2 L/2 1.1*L]]),
+        #         plot_points(geom0.values + dchi.values[:, 1:3]);
+        #         dims = 1)
+                plots = cat(plot_space_box([[-L/2 -L/2 0]; [L/2 L/2 1.1*L]]),
+                        plot_envelope(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield1.values, facecolor = "rgb(25, 255, 25)");
+                        dims = 1)
+        react!(pl, plots, pl.plot.layout)
+        sleep(0.15)
+    end
+    # for xscale in scale.*sin.(collect(0:1:5).*(2*pi/4))
+    #     println(xscale)
+    #     scattersysvec!(dchi, xscale.*v[:, 1])
+    #     Rfield1 = deepcopy(Rfield0)
+    #     update_rotation_field!(Rfield1, dchi)
+    #     for i in 1:length(pl.plot.data)
+    #         deletetraces!(pl, i)
+    #     end
+    #     println("length(pl.plot.data) = $(length(pl.plot.data))")
+    #     # plots = cat(plot_space_box([[-L/2 -L/2 0]; [L/2 L/2 1.1*L]]),
+    #     #     plot_nodes(fens),
+    #     #     plot_envelope(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield1.values);
+    #     #     dims = 1)
+    #         plots = cat(plot_space_box([[-L/2 -L/2 0]; [L/2 L/2 1.1*L]]),
+    #             plot_points(dchi.values[:, 1:3]);
+    #             dims = 1)
+    #     for tr in plots
+    #         addtraces!(pl, 1, tr)
+    #     end
+    #     println("length(pl.plot.data) = $(length(pl.plot.data))")
+    #     redraw!(pl)
+    #     sleep(5.0)
+    # end
+
+    return true
+end # argyr_l_frame_modal_anim
 
 function allrun()
     println("#####################################################")
