280624b026bee2e46abaac86aa4dffcf21435a11
Petr Krysl <pkrysl@ucsd.edu>
2020-02-16 19:45:23 -0800

    runner

--- go.jl
+++ go.jl
@@ -2,13 +2,17 @@
 using Pkg; Pkg.activate("."); Pkg.instantiate()
 
 
-include(".\\examples\\meshing\\mesh_examples.jl");   
-mesh_examples.curve_mesh_envelope()
-mesh_examples.argyr_l_frame_envelope()
+# include(".\\examples\\meshing\\mesh_examples.jl");   
+# mesh_examples.curve_mesh_envelope()
+# mesh_examples.argyr_l_frame_envelope()
+# mesh_examples.argyr_l_frame_movable()
 
-include(".\\examples\\modal\\beam_modal_examples.jl"); 
-beam_modal_examples.allrun()
+# include(".\\examples\\modal\\beam_modal_examples.jl"); 
+# beam_modal_examples.allrun()
 
 
-include(".\\examples\\modal\\argyr_l_frame_modal_examples.jl");   
-argyr_l_frame_modal_examples.allrun()
+# include(".\\examples\\modal\\argyr_l_frame_modal_examples.jl");   
+# argyr_l_frame_modal_examples.allrun()
+
+include(".\\examples\\linear_buckling\\tippling_examples.jl");  
+tippling_examples.tippling_1()
