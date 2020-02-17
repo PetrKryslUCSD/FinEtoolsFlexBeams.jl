47761ada2cadf2071dd2ca4ffb78242454659ac0
Petr Krysl <pkrysl@ucsd.edu>
2020-02-16 19:34:19 -0800

    more examples for mashing

--- examples/meshing/mesh_examples.jl
+++ examples/meshing/mesh_examples.jl
@@ -94,6 +94,41 @@ function argyr_l_frame()
     true
 end # argyr_l_frame
 
+function argyr_l_frame_movable()
+    # Parameters:
+    E=71240.0;#MPa
+    nu=0.31;# Poisson ratio
+    rho=5e-9;
+    b=3.0; h=30.0; L=240.0; # cross-sectional dimensions and length of each leg in millimeters
+
+    # Cross-sectional properties
+    cs = CrossSectionRectangle(s -> b, s -> h, s -> [0.0, 1.0, 0.0])
+
+    ## 
+    # Choose the mass formulation:
+    mass_type=2;
+
+    ## 
+    # Reference frequencies
+    reffs = [11.2732, 30.5269]
+    neigvs = 2;
+
+    # Select the number of elements per half the length.
+    xyz = 
+    n=1;
+    members = []
+    push!(members, frame_member([0 0 L; L 0 L], n, cs))
+    push!(members, frame_member([L 0 L; L 0 0], n, cs))
+    fens, fes = merge_members(members; tolerance = L / 10000);
+
+    plots = cat(plot_space_box([[0 -L/2 0]; [L L/2 L]]),
+        plot_nodes(fens), 
+        plot_midline(fens, fes; color = "rgb(155, 155, 255)", lwidth = 4), dims = 1)
+    p = render(plots; aspectratio = [1.0 1.0 1.0])
+    @show p.plot.data
+    true
+end # argyr_l_frame
+
 function argyr_l_frame_envelope()
     # Parameters:
     E=71240.0;#MPa
