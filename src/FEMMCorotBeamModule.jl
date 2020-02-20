e7e65565feb4630a92cdbb596ab2340505e9c0d1
Petr Krysl <pkrysl@ucsd.edu>
2020-02-19 18:58:28 -0800

    loading, restoring force

--- src/FEMMCorotBeamModule.jl
+++ src/FEMMCorotBeamModule.jl
@@ -4,7 +4,7 @@ using LinearAlgebra: norm, Transpose, mul!
 using FinEtools
 using FinEtools.IntegDomainModule: IntegDomain
 import FinEtoolsDeforLinear.MatDeforElastIsoModule: MatDeforElastIso
-using ..FESetCorotBeamModule: FESetL2CorotBeam, local_frame_and_def!, local_mass!, local_stiffness!, natural_forces!, local_geometric_stiffness!, MASS_TYPE_CONSISTENT_WITH_ROTATION_INERTIA
+using ..FESetCorotBeamModule: FESetL2CorotBeam, local_frame_and_def!, local_mass!, local_stiffness!, natural_forces!, local_geometric_stiffness!, local_forces!, MASS_TYPE_CONSISTENT_WITH_ROTATION_INERTIA
 
 """
     FEMMDeforLinear{MR<:AbstractDeforModelRed,  S<:AbstractFESet, F<:Function, M<:AbstractMatDeforLinearElastic} <: FEMMCorotBeam
@@ -17,12 +17,16 @@ mutable struct FEMMCorotBeam{S<:AbstractFESet, F<:Function} <: AbstractFEMM
     _ecoords0::FFltMat
     _ecoords1::FFltMat
     _edisp1::FFltMat
+    _evel1::FFltMat
+    _evel1f::FFltMat
     _dofnums::FIntMat
     _F0::FFltMat
     _Ft::FFltMat
     _FtI::FFltMat
     _FtJ::FFltMat
     _Te::FFltMat
+    _tempelmat1::FFltMat
+    _tempelmat2::FFltMat
     _elmat::FFltMat
     _elmatTe::FFltMat
     _elvec::FFltVec
@@ -108,6 +112,61 @@ function mass(self::FEMMCorotBeam, geom0::NodalField{FFlt}, u1::NodalField{T}, R
 end
 
 """
+    qmass(self::FEMMCorotBeam,  assembler::A,
+      geom::NodalField{FFlt},
+      u::NodalField{T}) where {A<:AbstractSysmatAssembler, T<:Number}
+
+Compute the quadratic-inertial term mass matrix
+
+This is a general routine for the abstract linear-deformation  FEMM.
+"""
+function qmass(self::FEMMCorotBeam, assembler::ASS, geom0::NodalField{FFlt}, u1::NodalField{T}, Rfield1::NodalField{T}, v1::NodalField{T}, dchi::NodalField{T}; mass_type=MASS_TYPE_CONSISTENT_WITH_ROTATION_INERTIA) where {ASS<:AbstractSysmatAssembler, T<:Number}
+    fes = self.integdomain.fes
+    ecoords0, ecoords1, edisp1, dofnums, F0, Ft, FtI, FtJ, Te, elmat, elmatTe, elvec, aN, dN, DN, PN, LF, R1I, R1J = _buffers(self)
+    evel1 = fill(0.0, 2, 6)
+    evel1f = fill(0.0, 2, 6)
+    Ge1 = deepcopy(elmat)
+    Ge2 = deepcopy(elmat)
+    Ge = deepcopy(elmat)
+    OmegaTilde = deepcopy(Te)
+    OS = deepcopy(F0)
+    rho = massdensity(self.material)
+    A, I1, I2, I3, x1x2_vector = fes.A, fes.I1, fes.I2, fes.I3, fes.x1x2_vector
+    startassembly!(assembler, size(elmat, 1), size(elmat, 2), count(fes), dchi.nfreedofs, dchi.nfreedofs);
+    for i = 1:count(fes) # Loop over elements
+        gathervalues_asmat!(geom0, ecoords0, fes.conn[i]);
+        gathervalues_asmat!(u1, edisp1, fes.conn[i]);
+        ecoords1 .= ecoords0 .+ edisp1
+        R1I[:] .= Rfield1.values[fes.conn[i][1], :];
+        R1J[:] .= Rfield1.values[fes.conn[i][2], :];
+        fill!(elmat,  0.0); # Initialize element matrix
+        L1, Ft, dN = local_frame_and_def!(Ft, dN, F0, FtI, FtJ, ecoords0, x1x2_vector[i], ecoords1, R1I, R1J);
+        L0 = norm(ecoords0[2,:]-ecoords0[1,:]); 
+        local_mass!(elmat, A[i], I1[i], I2[i], I3[i], rho, L0, mass_type);
+        _transfmat!(Te, Ft)
+        mul!(elmatTe, elmat, Transpose(Te))
+        mul!(elmat, Te, elmatTe)
+        gathervalues_asmat!(v1, evel1, fes.conn[i]);
+        evel1f[:, 1:3] = evel1[:,1:3]*Ft
+        evel1f[:, 4:6] = evel1[:,4:6]*Ft
+        Omega = (evel1f[1,4]+evel1f[2,4])/2*Ft[:,1] + (evel1f[1,3]-evel1f[2,3])/L1*Ft[:,2] + (evel1f[2,2]-evel1f[1,2])/L1*Ft[:,3];
+        skewmat!(OS, Omega);
+        _transfmat!(OmegaTilde, OS)
+        mul!(Ge1, OmegaTilde, elmat)
+        mul!(Ge2, elmat, OmegaTilde)
+        @. Ge = Ge1 - Ge2;
+        gatherdofnums!(dchi, dofnums, fes.conn[i]); # degrees of freedom
+        assemble!(assembler, Ge, dofnums, dofnums); 
+    end # Loop over elements
+    return makematrix!(assembler);
+end
+
+function qmass(self::FEMMCorotBeam, geom0::NodalField{FFlt}, u1::NodalField{T}, Rfield1::NodalField{T}, v1::NodalField{T}, dchi::NodalField{T}; mass_type=MASS_TYPE_CONSISTENT_WITH_ROTATION_INERTIA) where {T<:Number}
+    assembler = SysmatAssemblerSparseSymm();
+    return qmass(self, assembler, geom0, u1, Rfield1, v1, dchi; mass_type = mass_type);
+end
+
+"""
     stiffness(self::FEMMCorotBeam, assembler::ASS, geom0::NodalField{FFlt}, u1::NodalField{T}, Rfield1::NodalField{T}, dchi::NodalField{T}) where {ASS<:AbstractSysmatAssembler, T<:Number}
 
 Compute the material stiffness matrix.
@@ -184,13 +243,13 @@ end
 
 Compute the vector of the restoring elastic forces
 """
-function restoringforce(self::FEMMCorotBeam, assembler::ASS, geom0::NodalField{FFlt}, u1::NodalField{T}, Rfield1::NodalField{T}, dchi::NodalField{T}) where {ASS<:AbstractSysmatAssembler, T<:Number}
+function restoringforce(self::FEMMCorotBeam, assembler::ASS, geom0::NodalField{FFlt}, u1::NodalField{T}, Rfield1::NodalField{T}, dchi::NodalField{T}) where {ASS<:AbstractSysvecAssembler, T<:Number}
     fes = self.integdomain.fes
     ecoords0, ecoords1, edisp1, dofnums, F0, Ft, FtI, FtJ, Te, elmat, elmatTe, elvec, aN, dN, DN, PN, LF, R1I, R1J = _buffers(self)
     E = self.material.E
     G = E / 2 / (1 + self.material.nu)
     A, I2, I3, J, x1x2_vector = fes.A, fes.I2, fes.I3, fes.J, fes.x1x2_vector
-    startassembly!(assembler, size(elmat, 1), size(elmat, 2), count(fes), dchi.nfreedofs, dchi.nfreedofs);
+    startassembly!(assembler, dchi.nfreedofs);
     for i = 1:count(fes) # Loop over elements
         gathervalues_asmat!(geom0, ecoords0, fes.conn[i]);
         gathervalues_asmat!(u1, edisp1, fes.conn[i]);
@@ -213,5 +272,61 @@ function restoringforce(self::FEMMCorotBeam, geom0::NodalField{FFlt}, u1::NodalF
     return restoringforce(self, assembler, geom0, u1, Rfield1, dchi);
 end
 
+"""
+    distribloads_global(self::FEMMCorotBeam, geom0::NodalField{FFlt}, u1::NodalField{T}, Rfield1::NodalField{T}, dchi::NodalField{T}, fi) where {T<:Number}
+    
+Compute the load vector due to distributed loads.
+
+Compute the global load vector corresponding to applied distributed
+load. Here it means force per unit length of the beam,
+in the configuration u1,Rfield1.
+
+Note: the force intensity must be uniform across the entire element.
+Note: the force intensity is given in the global coordinates.
+"""
+function distribloads_global(self::FEMMCorotBeam, assembler::ASS, geom0::NodalField{FFlt}, u1::NodalField{T}, Rfield1::NodalField{T}, dchi::NodalField{T}, fi) where {ASS<:AbstractSysvecAssembler, T<:Number}
+    fes = self.integdomain.fes
+    ecoords0, ecoords1, edisp1, dofnums, F0, Ft, FtI, FtJ, Te, elmat, elmatTe, elvec, aN, dN, DN, PN, LF, R1I, R1J = _buffers(self)
+    elvecf = deepcopy(elvec)
+    Lforce = fill(0.0, 3)
+    ignore = fill( 0.0 , 0, 0)
+    E = self.material.E
+    G = E / 2 / (1 + self.material.nu)
+    A, I2, I3, J, x1x2_vector = fes.A, fes.I2, fes.I3, fes.J, fes.x1x2_vector
+    startassembly!(assembler, dchi.nfreedofs);
+    for i = 1:count(fes) # Loop over elements
+        gathervalues_asmat!(geom0, ecoords0, fes.conn[i]);
+        gathervalues_asmat!(u1, edisp1, fes.conn[i]);
+        ecoords1 .= ecoords0 .+ edisp1
+        R1I[:] .= Rfield1.values[fes.conn[i][1], :];
+        R1J[:] .= Rfield1.values[fes.conn[i][2], :];
+        fill!(elmat,  0.0); # Initialize element matrix
+        L1, Ft, dN = local_frame_and_def!(Ft, dN, F0, FtI, FtJ, ecoords0, x1x2_vector[i], ecoords1, R1I, R1J);
+        L0 = norm(ecoords0[2,:]-ecoords0[1,:]); 
+        force = updateforce!(fi, ignore, ignore, fes.label[i]); # retrieve the applied load
+        Lforce = Ft' * force # local distributed load components
+        elvecf[1] = Lforce[1]*L0/2;
+        elvecf[2] = Lforce[2]*L0/2;
+        elvecf[3] = Lforce[3]*L0/2;
+        elvecf[4] = 0;
+        elvecf[5] = -Lforce[3]*L0^2/12;
+        elvecf[6] = +Lforce[2]*L0^2/12;
+        elvecf[7] = Lforce[1]*L0/2;
+        elvecf[8] = Lforce[2]*L0/2;
+        elvecf[9] = Lforce[3]*L0/2;
+        elvecf[10] = 0;
+        elvecf[11] = +Lforce[3]*L0^2/12;
+        elvecf[12] = -Lforce[2]*L0^2/12;
+        mul!(elvec, Te, elvecf)
+        gatherdofnums!(dchi, dofnums, fes.conn[i]); # degrees of freedom
+        assemble!(assembler, elvec, dofnums); 
+    end # Loop over elements
+    return makevector!(assembler);
+end
+
+function distribloads_global(self::FEMMCorotBeam, geom0::NodalField{FFlt}, u1::NodalField{T}, Rfield1::NodalField{T}, dchi::NodalField{T}, fi) where {T<:Number}
+    assembler = SysvecAssembler();
+    return distribloads_global(self, assembler, geom0, u1, Rfield1, dchi, fi);
+end
 
 end # module
