86a755d2b5f66a1c245ca201bdf710a8a3d8fac7
Petr Krysl <pkrysl@ucsd.edu>
2020-02-24 14:57:02 -0800

    large-aspect ratio version

--- examples/nonlin_statics/curved_cantilever_examples.jl
+++ examples/nonlin_statics/curved_cantilever_examples.jl
@@ -198,6 +198,121 @@ function curved_cantilever()
     return true
 end # curved_cantilever
 
+function curved_cantilever_thin()
+    # Parameters:
+    E=1e7;# lb.in^-2
+    nu=0.3;
+    thinness_factor = 100
+    h= 1.0/thinness_factor; b = 1.0/thinness_factor;# in
+    Fmag=600.0/(thinness_factor^4);#  lb
+    radius=100;# in
+    ang=45;
+    nel=8;# number of elements
+    utol=1e-6;
+    maxit = 150
+    
+    # Cross-sectional properties
+    cs = CrossSectionRectangle(s -> b, s -> h, s -> [0.0, 0.0, 1.0])
+    
+    # Select the number of elements per leg.
+    fens, fes = frame_member([0 0 0; nel 0 0], nel, cs)
+    for i in 1:count(fens)
+        k = fens.xyz[i, 1]
+        fens.xyz[i, :] .= (radius*cos(k/nel*ang*2*pi/360), radius*sin(k/nel*ang*2*pi/360), 0)
+    end
+    clampedn = [1]
+    tipn = [nel+1];
+    
+    tolerance = radius/nel/100;
+    
+    # Material properties
+    material = MatDeforElastIso(DeforModelRed3D, 0.0, E, nu, 0.0)
+    
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+    
+    # Apply EBC's
+    for i in [1, 2, 3, 4, 5, 6]
+        setebc!(dchi, clampedn, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+    
+    # Additional fields
+    u1 = deepcopy(u0)
+    Rfield1 = deepcopy(Rfield0)
+    rhs = gathersysvec(dchi);
+    TMPv = deepcopy(rhs)
+    utol = 1e-13*dchi.nfreedofs;
+    
+    # tbox = plot_space_box([[0.0*radius 0.0*radius 0.0*radius]; [1.15*radius 0.75*radius 2.0*radius]])
+    # tshape0 = plot_solid(fens, fes; x = geom0.values, u = 0.0.*dchi.values[:, 1:3], R = Rfield0.values, facecolor = "rgb(125, 155, 125)", opacity = 0.3);
+    # plots = cat(tbox,  tshape0; dims = 1)
+    # pl = render(plots)
+    # sleep(0.5)
+    
+    femm = FEMMCorotBeam(IntegDomain(fes, GaussRule(1, 2)), material)
+    loadbdry = FESetP1(reshape(tipn, 1, 1))
+    lfemm = FEMMBase(IntegDomain(loadbdry, PointRule()))
+    
+    load_parameters = 0.0:0.05:1;
+    tip_displacement = fill(0.0, length(load_parameters), 3);
+    step = 0
+    for load_parameter in load_parameters
+        applyebc!(dchi) # Apply boundary conditions
+        u1.values[:] = u0.values[:]; # guess
+        Rfield1.values[:] = Rfield0.values[:]; # guess
+        fi = ForceIntensity(FFlt[0, 0, load_parameter*Fmag, 0, 0, 0]);
+        
+        println("Load: $load_parameter")
+        iter = 1;
+        while true
+            F = distribloads(lfemm, geom0, dchi, fi, 3);
+            Fr = restoringforce(femm, geom0, u1, Rfield1, dchi);       # Internal forces
+            @. rhs = F + Fr;
+            K = stiffness(femm, geom0, u1, Rfield1, dchi) + geostiffness(femm, geom0, u1, Rfield1, dchi);
+            dchi = scattersysvec!(dchi, (K)\rhs); # Disp. incr
+            u1.values[:] += (dchi.values[:,1:3])[:];   # increment displacement
+            update_rotation_field!(Rfield1, dchi)
+            print("$iter: ||du||=$(maximum(abs.(dchi.values[:])))\n")
+            if maximum(abs.(dchi.values[:])) < utol # convergence check
+                break;
+            end
+            if (iter > maxit)# bailout for failed convergence
+                error("Possible failed convergence");
+            end
+            iter += 1;
+        end
+        u0.values[:] = u1.values[:];       # update the displacement
+        Rfield0.values[:] = Rfield1.values[:]; # update the rotations
+        
+        
+        step = step + 1
+        tip_displacement[step, :] .= u1.values[tipn[1], :]
+
+    end
+    step = step + 1
+    
+    plots = cat(
+    scatter(;x=tip_displacement[:,1], y=load_parameters.*Fmag, mode="lines", line_color = "rgb(0, 0, 0)", name="Tip-x"),
+    scatter(;x=tip_displacement[:,2], y=load_parameters.*Fmag, mode="lines", line_color = "rgb(0, 0, 0)", name="Tip-y"),
+    scatter(;x=tip_displacement[:,3], y=load_parameters.*Fmag, mode="lines", line_color = "rgb(0, 0, 0)", name="Tip-z"),
+    scatter(;x=izzelna_ux[:,1], y=izzelna_ux[:,2]/(thinness_factor^4), mode="markers", line_color = "rgb(255, 0, 0)", name="Ref-x"),
+    scatter(;x=izzelna_uy[:,1], y=izzelna_uy[:,2]/(thinness_factor^4), mode="markers", line_color = "rgb(0, 255, 0)", name="Ref-y"),
+    scatter(;x=izzelna_uz[:,1], y=izzelna_uz[:,2]/(thinness_factor^4), mode="markers", line_color = "rgb(0, 0, 255)", name="Ref-z"); 
+    dims = 1)
+    layout = Layout(width=700, height=500, xaxis=attr(title="Tip displacement [in]"),
+    yaxis=attr(title="Load [lb]"))
+    pl = plot(plots, layout)
+    display(pl)
+    
+    return true
+end # curved_cantilever
+
 function allrun()
     println("#####################################################")
     println("# slowtop1 ")
