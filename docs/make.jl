8ea3d23578a49fb7488eb1f0f11f0603ef5cacf4
Petr Krysl <pkrysl@ucsd.edu>
2020-07-27 13:34:41 -0700

    documentation

---
+++ docs/make.jl
@@ -0,0 +1,20 @@
+using Documenter, FinEtools, FinEtoolsFlexBeams
+
+makedocs(
+	modules = [FinEtoolsFlexBeams],
+	doctest = false, clean = true,
+	format = Documenter.HTML(prettyurls = false),
+	authors = "Petr Krysl",
+	sitename = "FinEtoolsFlexBeams.jl",
+	pages = Any[
+	"Home" => "index.md",
+	"Guide" => "guide/guide.md",
+	"Types and Functions" => Any[
+		"man/types.md",
+		"man/functions.md"]
+		]
+	)
+
+deploydocs(
+    repo = "github.com/PetrKryslUCSD/FinEtoolsFlexBeams.jl.git",
+)
---
+++ docs/src/guide/guide.md
@@ -0,0 +1,3 @@
+# Guide
+
+The [`FinEtools`](https://petrkryslucsd.github.io/FinEtools.jl/latest/index.html) package is used here to solve static and dynamic linear and nonlinear  problems involving beams.
