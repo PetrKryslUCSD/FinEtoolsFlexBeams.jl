f40e160cbb669271f18225560aad5dc13a60b642
Petr Krysl <pkrysl@ucsd.edu>
2020-08-05 09:12:17 -0700

    separated geometry

--- tutorials/garteur_hva_tut.jl
+++ tutorials/garteur_hva_tut.jl
@@ -44,135 +44,13 @@
 
 # ![](garteur-geom.png)
 
-# The beam finite element code relies on the basic functionality implemented in this
-# package.
-using FinEtools
+# The script included below defines the geometry of the structure, the
+# cross-sectional properties, the connectivity, and the location of the nodes.
+include("garteur_geometry_tut.jl")
 
-# This is the characteristic length. The dimensions of the aircraft frame are
-# expressed in terms of multiples of this characteristic unit.
-L = 0.1*phun("m");
+# The geometry is visualized in the tutorial 
+# [garteur_geometry_vis_tut](garteur_geometry_vis_tut.jl).
 
-##
-# ## Cross-section
-
-# Cross-sectional properties are incorporated in the cross-section property.
-# There are several rectangular cross-sections in the model: the fuselage, the
-# wing, the tail. There are also three massless connectors: connections between
-# the fuselage and the wing, between the wing structure and the viscoelastic
-# damping layer, and between the fuselage and the tail.
-using FinEtoolsFlexBeams.CrossSectionModule: CrossSectionRectangle
-# Body of the frame.
-cs_body = CrossSectionRectangle(s -> 1.5*L, s -> L/2, s -> [1.0, 0.0, 1.0])
-# Wing beam.
-cs_wing = CrossSectionRectangle(s -> L/10, s -> L, s -> [0.0, 0.0, 1.0])
-# Wing drums.
-cs_drum = CrossSectionRectangle(s -> L/10, s -> L, s -> [0.0, 0.0, 1.0])
-# Vertical part of the tail.
-cs_tailv = CrossSectionRectangle(s -> L, s -> L/10, s -> [1.0, 0.0, 1.0])
-# Horizontal part of the tail.
-cs_tailh = CrossSectionRectangle(s -> L/10, s -> L, s -> [0.0, 0.0, 1.0])
-# Massless connectors of the structural parts.
-cs_conn = CrossSectionRectangle(s -> L/5, s -> L/5, s -> [1.0, 0.0, 1.0])
-# Massless connector between the body and the wings.
-cs_connw = CrossSectionRectangle(s -> L/2, s -> L, s -> [0.0, 1.0, 0.0])
-# Massless connector between the body and the tail.
-cs_connt = CrossSectionRectangle(s -> L, s -> L/5, s -> [1.0, 0.0, 1.0])
-# Viscoelastic connecting layer.
-cs_vconstr = CrossSectionRectangle(s -> L*(1.1/100), s -> L*(76.2/100), s -> [0.0, 0.0, 1.0])
-
-# ## Mesh 
-
-# We shall use this utility function to generate the mesh of the individual
-# parts. This will result in a number of separate meshes for the members. These
-# separate meshes will then be glued together (merged) based on the tolerance on
-# the location of the nodes.
-using FinEtoolsFlexBeams.MeshFrameMemberModule: frame_member
-tolerance = L/10000;
-# Number of intervals from 0.25*L to 8.5*L (the extent of the constraining plate).
-nc = 8
-meshes = Tuple{FENodeSet, AbstractFESet}[]
-
-# Define the constituent parts of the body of the aircraft.
-push!(meshes, frame_member([-9*L 0 0; -8.5*L 0 0], 1, cs_body; label = 1))
-push!(meshes, frame_member([-8.5*L 0 0; -8.0*L 0 0], 1, cs_body; label = 1))
-push!(meshes, frame_member([-8.0*L 0 0; -2.0*L 0 0], 2, cs_body; label = 1))
-push!(meshes, frame_member([-2.0*L 0 0; 0 0 0], 1, cs_body; label = 1))
-push!(meshes, frame_member([0 0 0; 6*L 0 0], 2, cs_body; label = 1))
-
-# Define the aluminum parts of the wings.
-push!(meshes, frame_member([0 0 0.805*L;  0 0.25*L 0.805*L], 1, cs_wing; label = 2))
-push!(meshes, frame_member([0 0 0.805*L;  0 -0.25*L 0.805*L], 1, cs_wing; label = 2))
-push!(meshes, frame_member([0 0.25*L 0.805*L;  0 8.5*L 0.805*L], nc, cs_wing; label = 2))
-push!(meshes, frame_member([0 -0.25*L 0.805*L;  0 -8.5*L 0.805*L], nc, cs_wing; label = 2))
-push!(meshes, frame_member([0 8.5*L 0.805*L;  0 9.5*L 0.805*L], 1, cs_wing; label = 2))
-push!(meshes, frame_member([0 -8.5*L 0.805*L;  0 -9.5*L 0.805*L], 1, cs_wing; label = 2))
-push!(meshes, frame_member([0 9.5*L 0.805*L;  0 10.0*L 0.805*L], 1, cs_wing; label = 2))
-push!(meshes, frame_member([0 -9.5*L 0.805*L;  0 -10.0*L 0.805*L], 1, cs_wing; label = 2))
-
-# Define the drums at the ends of the wings.
-push!(meshes, frame_member([0 +9.5*L +0.91*L; +2*L +9.5*L +0.91*L], 1, cs_drum; label = 3))
-push!(meshes, frame_member([0 +9.5*L +0.91*L; -2*L +9.5*L +0.91*L], 1, cs_drum; label = 3))
-push!(meshes, frame_member([0 -9.5*L +0.91*L; +2*L -9.5*L +0.91*L], 1, cs_drum; label = 3))
-push!(meshes, frame_member([0 -9.5*L +0.91*L; -2*L -9.5*L +0.91*L], 1, cs_drum; label = 3))
-
-# Define the horizontal and vertical parts of the tail.
-push!(meshes, frame_member([-8*L 0 .75*L; -8*L 0 3.35*L], 2, cs_tailv; label = 4))
-push!(meshes, frame_member([-8*L 0 3.35*L; -8*L 0 3.75*L], 2, cs_tailv; label = 4))
-push!(meshes, frame_member([-8*L 0 3.8*L; -8*L 2*L 3.8*L], 2, cs_tailh; label = 5))
-push!(meshes, frame_member([-8*L 0 3.8*L; -8*L -2*L 3.8*L], 2, cs_tailh; label = 5))
-
-# Define the parts of the aluminum constraining plate for the viscoelastic layer.
-push!(meshes, frame_member([-.119*L 0 0.8665*L;  -.119*L 0.25*L 0.8665*L], 1, cs_vconstr; label = 6))
-push!(meshes, frame_member([-.119*L 0 0.8665*L;  -.119*L -0.25*L 0.8665*L], 1, cs_vconstr; label = 6))
-push!(meshes, frame_member([-.119*L 0.25*L 0.8665*L;  -.119*L 8.5*L 0.8665*L], nc, cs_vconstr; label = 6))
-push!(meshes, frame_member([-.119*L -0.25*L 0.8665*L;  -.119*L -8.5*L 0.8665*L], nc, cs_vconstr; label = 6))
-
-
-# Define the massless connectors between:
-# Wing - Wingdrum
-push!(meshes, frame_member([0 +9.5*L +0.805*L;0 +9.5*L +0.91*L], 1, cs_conn; label = 7))
-push!(meshes, frame_member([0 -9.5*L +0.805*L;0 -9.5*L +0.91*L], 1, cs_conn; label = 7))
-# Body-Wing
-push!(meshes, frame_member([0 0 0; 0 0.0*L .805*L], 1, cs_connw; label = 7))
-# Body-Tail
-push!(meshes, frame_member([-8*L 0 0; -8*L 0 .75*L], 1, cs_connt; label = 7))
-# Tail-Taildrum
-push!(meshes, frame_member([-8*L 0 3.75*L; -8*L 0 3.8*L], 1, cs_conn; label = 7))
-
-# Wing-Constraining plate for the viscoelastic layer
-# Middle connector created individually
-push!(meshes, frame_member([0 0 .805*L; -.119*L 0 0.8665*L], 1, cs_conn; label = 7))
-
-# Connectors created for both wings
-for i in 1:nc+1
-    push!(meshes, frame_member([0 (0.25+(i-1)*8.25/nc)*L .805*L;  -.119*L (0.25+(i-1)*8.25/nc)*L 0.8665*L], 1, cs_conn; label = 7))
-    push!(meshes, frame_member([0 -(0.25+(i-1)*8.25/nc)*L .805*L;  -.119*L -(0.25+(i-1)*8.25/nc)*L 0.8665*L], 1, cs_conn; label = 7))
-end
-
-# Massless Sensor Connectors
-# Tail Sensors
-push!(meshes, frame_member([-8*L 2*L 3.8*L; -(153/20)*L (37/20)*L 3.85*L], 1, cs_conn; label = 8))# 303
-push!(meshes, frame_member([-8*L -2*L 3.8*L; -(153/20)*L -(37/20)*L 3.85*L], 1, cs_conn; label = 8))# 301
-
-# Wingdrum Sensors
-push!(meshes, frame_member([0 9.5*L .91*L ; 0 9.8*L .96*L], 1, cs_conn; label = 8))# 101
-push!(meshes, frame_member([-2*L 9.5*L .91*L ; -1.8*L 9.8*L .96*L], 1, cs_conn; label = 8))# 112
-push!(meshes, frame_member([2*L 9.5*L .91*L ; 1.8*L 9.8*L .96*L], 1, cs_conn; label = 8))# 111
-
-push!(meshes, frame_member([0 -9.5*L .91*L ; 0 -9.8*L .96*L], 1, cs_conn; label = 8))# 1
-push!(meshes, frame_member([-2*L -9.5*L .91*L ; -1.8*L -9.8*L .96*L], 1, cs_conn; label = 8))# 12
-push!(meshes, frame_member([2*L -9.5*L .91*L ; 1.8*L -9.8*L .96*L], 1, cs_conn; label = 8))# 11
-
-# Wingdrum complementary masses
-push!(meshes, frame_member([2*L 9.5*L .91*L ; 1.8*L 9.2*L .96*L], 1, cs_conn; label = 8))# added mass
-push!(meshes, frame_member([2*L -9.5*L .91*L ; 1.8*L -9.2*L .96*L], 1, cs_conn; label = 8))# added mass
-
-# Merge all the meshes of individual parts. This will glue together nodes which
-# are in the "same" location. The parts of the mesh can be distinguished based
-# on the label. 
-fens, fesa = mergenmeshes(meshes, tolerance)
-
-# The geometry is visualized in the tutorial [garteur_geometry_tut](garteur_geometry_tut.jl).
 
 ##
 # ## Material
@@ -188,29 +66,14 @@ nu = 0.31;
 rho = 2700 * phun("kg/m^3")
 alu = MatDeforElastIso(DeforModelRed3D, rho, E, nu, 0.0)
 
-# The material of the viscoelastic layer. The properties are due to the
-# technical specification [3]. Since the properties are frequency dependent, we
-# take as a representative value the numbers obtained for 20 Hz.
-# Poisson ratio:
-nu = 0.49;
-# Storage modulus:
-Gp = 0.6 * phun("MPa") 
-E = 2 * (1 + nu) * Gp
-# The mass density:
-rho = 900 * phun("kg/m^3")
-layer = MatDeforElastIso(DeforModelRed3D, rho, E, nu, 0.0)
-
 # Material for the massless connectors has the mass density set to zero;
 # otherwise it has the same properties as the aluminum material  of the
 # structure.
-
 massless = MatDeforElastIso(DeforModelRed3D, 0.0, alu.E, alu.nu, 0.0)
 
 # This simple function returns material based on the label of the beam elements.
 material(labl) = begin
-    if labl == 6
-        return layer
-    elseif labl == 7 || labl == 8
+    if labl >= 7 
         return massless
     end
     return alu
@@ -281,12 +144,14 @@ end
 # ## Assemble the global discrete system
 
 
-# For disambiguation we will refer to the stiffness and mass functions by qualifying them with the corotational-beam module, `FEMMCorotBeamModule`.
+# For disambiguation we will refer to the stiffness and mass functions by
+# qualifying them with the corotational-beam module, `FEMMCorotBeamModule`.
 using FinEtoolsFlexBeams.FEMMCorotBeamModule
 CB = FEMMCorotBeamModule
 
-# Note that we have an array of finite element sets. We compute the matrices for each set separately and accumulate them into the final overall matrix.
-# Thus we can construct the stiffness and mass matrix as follows.
+# Note that we have an array of finite element sets. We compute the matrices for
+# each set separately and accumulate them into the final overall matrix. Thus
+# we can construct the stiffness and mass matrix as follows.
 using  SparseArrays
 
 Kf, Kd, M = let
@@ -296,7 +161,7 @@ Kf, Kd, M = let
     for fes in fesa
         labl  = fes.label[1]
         femm = CB.FEMMCorotBeam(IntegDomain(fes, GaussRule(1, 2)), material(labl));
-        if labl == 6 # damping layer
+        if labl == 7 # connectors representing the damping layer
             Kd += CB.stiffness(femm, geom0, u0, Rfield0, dchi);
         else
             Kf += CB.stiffness(femm, geom0, u0, Rfield0, dchi);
@@ -318,7 +183,8 @@ PM = FEMMPointMassModule
 # There is a sensor on the tail.
 femmcm1 =  PM.FEMMPointMass(IntegDomain(FESetP1(reshape([sensor202n;], 1, 1)), PointRule()), FFltMat(2*L*L/5*L/5*2*rho*I(3)));
 
-# These are the forward/interior locations on the wing drums.
+# These are the forward/interior locations on the wing drums where the
+# compensation masses are attached.
 mass1n = selectnode(fens; box = initbox!(Float64[], vec([1.8*L 9.2*L .96*L])), inflate = tolerance)
 mass2n = selectnode(fens; box = initbox!(Float64[], vec([1.8*L -9.2*L .96*L])), inflate = tolerance)
 femmcm2 =  PM.FEMMPointMass(IntegDomain(FESetP1(reshape([mass1n; mass2n;], 2, 1)), PointRule()), FFltMat(0.2*phun("kg")*I(3)));
@@ -345,22 +211,31 @@ Kb = BS.stiffness(femmbs, geom0, u0, Rfield0, dchi)
 ##
 # ## Damping
 
-# The measured overall damping ratios were approximately 1%. Damping levels
-# thought appropriate for obtaining realistic damping were obtained through the
-# use of a viscoelastic layer with an aluminum constraining layer. 
-
+# The overall damping ratios measured in the physical experiments were
+# approximately 1%. Damping levels thought appropriate for obtaining realistic
+# damping were obtained through the use of a viscoelastic layer glued to the
+# wing beam with an aluminum constraining plate on top. 
 
 
 # ### Viscoelastic damping layer
 
+# The properties of the material of the viscoelastic layer were described in the
+# technical specification [3]. Since the properties are frequency dependent, we
+# take as a representative value the numbers obtained for 20 Hz.
+
 # The viscoelastic used was the 3M acrylic viscoelastic polymer ISD 112 in the
-# form of a 76 mm by 50 mm roll.  This material is particularly well suited for
-# the testbed operating range of 5-50 Hz and at 20 degrees C where the loss
-# factor is near its peak of 0.4 [3]. These are representative quantities taken
-# at 20 Hz.
+# form of a tape of 76 mm width with thickness of 50 microm.  This material is
+# particularly well suited for the testbed operating range of 5-50 Hz and at 20
+# degrees C where the loss factor is near its peak of 0.4 [3]. These are
+# representative quantities taken at 20 Hz.
 
 eta = 0.5
 omega_5_50 = 2*pi*20
+
+
+# The connectors between the wing beam and the constraining plate are taken as
+# representative of the stiffness of the constraining layer. The viscoelastic
+# damping is then taken as proportional to this stiffness.
 Cd = (eta / omega_5_50) .* Kd
 
 # ### Aluminum structure
@@ -404,7 +279,7 @@ F = CB.distribloads(lfemm, geom0, dchi, fi, 3);
 # ## Solve the Harmonic vibration problem
 
 fromf = 3.0
-tof = 50.0
+tof = 70.0
 nf = 150
 
 frequencies = logspace(log10(fromf), log10(tof), nf);
