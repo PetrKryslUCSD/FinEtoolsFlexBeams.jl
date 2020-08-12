9180af475df623ec3cb51afeef97b4b66b106f82
Petr Krysl <pkrysl@ucsd.edu>
2020-08-11 20:04:13 -0700

    move tutorials

--- docs/src/tutorials/make.jl
+++ docs/src/tutorials/make.jl
@@ -5,5 +5,8 @@ for t in readdir(".")
         println("\nTutorial $t in $(pwd())\n")
         Literate.markdown(t, "."; documenter=false);
         # Literate.notebook(t, "."; execute=false, documenter=false);
+        # @show n, ext = splitext(t)
+        # @show "../docs/tutorials/" * n * ".md"
+        # cp(n * ".md", "../docs/src/tutorials/" * n * ".md", force = true)
     end
 end
